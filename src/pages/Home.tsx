66d4c430c011629906f208e4884c33c469552bf3
Angelo <ancricoma_angelo@hotmail.com>
2024-11-19 22:37:35 -0500

    added button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { UseCharacterStore } from "@/services/UseApi";
 
 function App() {
@@ -30,30 +31,40 @@ function App() {
     return <div className="text-center mt-8 text-red-500">Error: {error}</div>;
 
   return (
-    <div className=" mx-auto px-4 py-8">
+    <div className="mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center text-white">
         Rick and Morty Explorer
       </h1>
-      <div className="relative max-w-sm mx-auto mb-8  ">
-        <Input
-          type="text"
-          placeholder="Filter characters..."
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          className="pr-7 text-white"
-        />
-        <span className="absolute inset-y-0 right-2 flex items-center text-gray-500">
-          🔍
-        </span>
+      <div className="flex items-center max-w-sm mx-auto mb-8">
+        <div className="flex-1 relative">
+          <Input
+            type="text"
+            placeholder="Filter characters..."
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="pr-7 text-white"
+          />
+          <span className="absolute inset-y-0 right-2 flex items-center text-gray-500">
+            🔍
+          </span>
+        </div>
+        <div className="flex gap-2 ml-2">
+          <Button variant="default" className="text-white">
+            Add
+          </Button>
+          <Button variant="destructive" className="text-white">
+            Edit
+          </Button>
+        </div>
       </div>
-      <div className="sm:mx-10 lg:mx-20 xl:mx-44  grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="sm:mx-10 lg:mx-20 xl:mx-44 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {filteredCharacters.map((character) => (
           <Card key={character.id} className="shadow-md bg-card">
             <CardHeader>
               <img
                 src={character.image}
                 alt={character.name}
-                className="rounded-lg h-full object-cover w-full "
+                className="rounded-lg h-full object-cover w-full"
               />
             </CardHeader>
             <CardContent>
